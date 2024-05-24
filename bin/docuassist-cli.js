fdbe6000e067a5709c5e3bc8454e7f9b2515fd0b
Shantanu Vishwanadha <mailvishwanadha@gmail.com>
2024-05-24 16:12:26 +0530

    Added CLI Commands and Lib files

---
+++ bin/docuassist-cli.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+const { program } = require('commander');
+const { initPlugin, ingestDocs, CheckTest } = require('../lib/commands');
+
+program
+  .command('plugin init')
+  .description('Install plugin in the Docusaurus directory')
+  .action(initPlugin);
+
+program
+  .command('ingest')
+  .description('Ingest documents into the vector store')
+  .action(ingestDocs);
+
+program
+  .command('check')
+  .description('Check test')
+  .action(CheckTest);
+
+program.parse(process.argv);
